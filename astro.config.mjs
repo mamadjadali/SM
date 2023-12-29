7261c1d3323f3f3740bbbc034fa3d7d069bfaf71
Mamad Jadali <jadalimamad@gmail.com>
2023-12-29 23:32:48 +0330

    v1.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site : "https://numonu.github.io",
-  base : "/Luxury-Architect"
+  site: "https://mamadjadali.github.io",
+  base: "/Luxury-Architect"
 });
